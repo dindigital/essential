3f399bb474a06c5a07491c4b8e9859f024c9c80c
Jair Cueva Junior <junior@dinhost.com.br>
2023-08-29 10:52:44 -0300

    feat(relationship): select2 com tabela arquivavel

--- library/Din/Essential/Models/RelationshipModel.php
+++ library/Din/Essential/Models/RelationshipModel.php
@@ -88,10 +88,6 @@ class RelationshipModel extends BaseModelAdm
       $arrCriteria['is_del = ?'] = '0';
     }
 
-    if ( $this->_foreign_entity->isArchivable() ) {
-      $arrCriteria['is_archivable = ?'] = '0';
-    }
-
     $select = new Select($fe_tbl);
     $select->addField($fe_id, 'id');
     $select->addField($fe_title, 'text');
